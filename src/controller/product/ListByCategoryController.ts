5f053286bf98883b16afebe5f78d858c0514c182
mr-louback <loubacked4work@outlook.com>
2023-11-12 15:59:36 -0300

    commit empty

--- src/controller/product/ListByCategoryController.ts
+++ src/controller/product/ListByCategoryController.ts
@@ -2,10 +2,10 @@ import { Response, Request } from "express";
 import { ListByCategoryService } from "../../service/products/ListByCategoryService";
 class ListByCategoryController {
   async handle(res: Response, req: Request) {
-    const category_id  = req.query.category_id as string;
     const listByCategoryService = new ListByCategoryService();
-    const listByCategory = await listByCategoryService.execute({ category_id });  
-    return res.json(listByCategory);
+    const minhafucao = await listByCategoryService.execute();
+    
+    return res.json({minhafucao});
   }
 }
 export { ListByCategoryController }
--- src/service/products/ListByCategoryService.ts
+++ src/service/products/ListByCategoryService.ts
@@ -1,33 +1,27 @@
 import prismaClient from "../../prisma";
+import { Response } from 'express'
 interface ListRequest {
   category_id: string;
 }
-interface ListResponse {
-  id: string;
-  name: string;
-  description: string;
-  price: string;
-  banner: string;
-  category_id: string;
-  items: string;
-}
+
 class ListByCategoryService {
-  async execute({ category_id }: ListRequest) {
-    const listByCategory = await prismaClient.product.findMany({
-      where: {
-        category_id
-      },
-      select: {
-        id: true,
-        name: true,
-        description: true,
-        price: true,
-        banner: true,
-        category_id: true,
-        items: true,
-      }
-    })
-    return listByCategory;
+  async execute() {
+    // const listByCategory = await prismaClient.product.findMany({
+    //   where: {
+    //     category_id
+    //   },
+    //   select: {
+    //     id: true,
+    //     name: true,
+    //     description: true,
+    //     price: true,
+    //     banner: true,
+    //     items: true,
+    //   }
+    // })
+    // return listByCategory;
+    return { ok: 'true' }
+
 
 
   }
